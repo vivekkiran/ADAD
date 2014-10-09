0605b9b0715684e62d91c1e08eb1e6c8446a75dc
Vivek Kiran <vivek.kiran@Outlook.com>
2014-10-09 22:23:14 +0530

    Init Commit

---
+++ gen/com/adad/emergecy/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.adad.emergecy;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/adad/emergecy/MainActivity.java
@@ -0,0 +1,135 @@
+package com.adad.emergecy;
+
+import java.util.Locale;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends FragmentActivity implements LocationListener {
+
+	GoogleMap googleMap;
+
+	// TextView content;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		StrictMode.ThreadPolicy tp = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		StrictMode.setThreadPolicy(tp);
+
+		Button saveme = (Button) findViewById(R.id.send);
+		// SupportMapFragment fm = (SupportMapFragment)
+		// getSupportFragmentManager().findFragmentById(R.id.map);
+		// googleMap = fm.getMap();
+		// googleMap.setMyLocationEnabled(true);
+		SupportMapFragment fm = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+		LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+		// Creating a criteria object to retrieve provider
+		Criteria criteria = new Criteria();
+
+		// Getting the name of the best provider
+		String provider = locationManager.getBestProvider(criteria, true);
+
+		// Getting Current Location
+		final Location location = locationManager.getLastKnownLocation(provider);
+		googleMap = fm.getMap();
+		googleMap.setMyLocationEnabled(true);
+		googleMap.isTrafficEnabled();
+		googleMap.setTrafficEnabled(true);
+		googleMap.isMyLocationEnabled();
+
+		saveme.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
+				// ALERT MESSAGE
+				Toast.makeText(getBaseContext(), "Please wait, connecting to server.", Toast.LENGTH_LONG).show();
+
+				HttpClient Client = new DefaultHttpClient();
+
+				// Create URL string
+
+				String URL = "http://172.16.250.147/compare.php?Latitude=" + location.getLatitude() + "&Longitude="
+						+ location.getLatitude();
+
+				// Log.i("httpget", URL);
+
+				try {
+					String SetServerString = "";
+
+					// Create Request to server and get response
+
+					HttpGet httpget = new HttpGet(URL);
+					ResponseHandler<String> responseHandler = new BasicResponseHandler();
+					SetServerString = Client.execute(httpget, responseHandler);
+
+					// Show response on activity
+
+				} catch (Exception ex) {
+				}
+			}
+		});
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+
+		// Getting latitude of the current location
+		double latitude = location.getLatitude();
+
+		// Getting longitude of the current location
+		double longitude = location.getLongitude();
+
+		// Creating a LatLng object for the current location
+		LatLng latLng = new LatLng(latitude, longitude);
+
+		// Showing the current location in Google Map
+		googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+		// Zoom in the Google Map
+		googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+		// // Setting latitude and longitude in the TextView tv_location
+		// tvLocation.setText("Latitude:" + latitude + ", Longitude:"+ longitude
+		// );
+
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+	}
+}
