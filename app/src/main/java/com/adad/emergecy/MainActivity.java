83047c937c1da882f77c7985eaba029b138f0afe
Vivek Kiran <vivek.kiran@outlook.com>
2015-04-16 06:48:07 +0530

    migrated project to Android Studio

--- app/src/main/java/com/adad/emergecy/MainActivity.java
+++ app/src/main/java/com/adad/emergecy/MainActivity.java
@@ -18,6 +18,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
